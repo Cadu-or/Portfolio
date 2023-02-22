3be398f66fe44e81f685ce8da0e516983fd22146
Carlos Eduardo <carlos.e.o.ribeiro@gmail.com>
2023-02-22 01:13:38 -0300

    ✨ Adicionar rota de projetos.

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,11 +2,13 @@ import { Route, Routes } from "react-router-dom";
 import { Curriculo } from "./pages/Curriculo";
 import { Habilidades } from "./pages/Habilidades";
 import { Inicio } from "./pages/Inicio";
+import { Projetos } from "./pages/Projetos";
 
 export function Router(){
   return (
     <Routes>
       <Route path="/" element={<Inicio />} />
+      <Route path="/projetos" element={<Projetos />} />
       <Route path="/curriculo" element={<Curriculo/>} />
       <Route path="/habilidades" element={<Habilidades />} />
     </Routes>
